190770bc61917374d3f634ceb5efec4082d6b2ba
tishka989 <ves16091980@gmail.com>
2025-03-27 20:40:45 +0500

    Добавлена новая страница каталога

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
+
+    const{cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext)
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+            if(cartItems[item._id]>0)
+            {
+                return(
+                  
+                  <div>
+                     <div className='cart-items-title cart-items-item'>
+                     <img src={item.image} alt="" />
+                     <p>{item.name}</p>
+                     <p>{item.price}₸</p>
+                     <p>{cartItems[item._id]}</p>
+                     <p>{item.price*cartItems[item._id]}₸</p>
+                     <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
+                  </div>
+                  <hr />
+                  </div>
+                 
+                )
+            }
+
+        })}
+      </div>
+      <div className='cart-bottom'>
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-detail">
+              <p>Subtotal</p>
+              <p>{getTotalCartAmount()}₸</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <p>Delivery</p>
+              <p>{2}₸</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <b>Total</b>
+              <b>{getTotalCartAmount()+2}₸</b>
+            </div>
+           
+          </div>
+          <button>proceed to checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>if u have promocode, enter it here</p>
+            <div className='cart-promocode-input'>
+                <input type="text" placeholder='promocode' />
+                <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
